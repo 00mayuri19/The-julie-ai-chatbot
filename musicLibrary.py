52c622e7a73e6a7a5217cddfc8f846d080a984c6
00mayuri19 <164994767+00mayuri19@users.noreply.github.com>
2025-09-02 14:02:58 +0530

    Create musicLibrary.py

---
+++ musicLibrary.py
@@ -0,0 +1,6 @@
+music = {
+    "mann mera": "https://www.youtube.com/watch?v=SlHnlxDt2TQ&list=RDSlHnlxDt2TQ&start_radio=1&pp=ygUVbWFubiBtZXJhIHRhYmxlIG5vIDIxoAcB",
+    "ashiq tera": "https://www.youtube.com/watch?v=emh8zR4ZqZ0&list=RDemh8zR4ZqZ0&start_radio=1&pp=ygUJYXNoaXF0ZXJhoAcB",
+    "azul": "https://www.youtube.com/watch?v=vB0V3iCSzQw&list=RDvB0V3iCSzQw&start_radio=1&pp=ygUEYXp1bKAHAdIHCQmyCQGHKiGM7w%3D%3D",
+    "wolf": "https://www.youtube.com/watch?v=ThCH0U6aJpU&list=PLnrGi_-oOR6wm0Vi-1OsiLiV5ePSPs9oF&index=21"
+}
